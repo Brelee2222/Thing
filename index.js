b806270cf1d4f0b365c131a83d258afd3d5538cf
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2023-04-24 21:01:39 -0700

    initial

---
+++ index.js
@@ -0,0 +1,17 @@
+const app = require("express")();
+const fs = require("fs");
+const port = 3000;
+
+app.get("/json", (req, res) => {
+    res.send(require("./my.json"))
+});
+
+app.get("/", (res) => {
+    res.send(fs.readFileSync("index.html", "utf-8"));
+})
+
+app.get("/img", (req, res) => {
+    res.send(`<img src="data:image/png;base64,${fs.readFileSync("./T0ALL74SY-UMWEQAGV8-0673c96e7f8b-512.png", "base64")}">`);
+});
+
+app.listen(port)
